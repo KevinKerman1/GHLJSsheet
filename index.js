db1f8e66f3d16a602869194fbf3256807f528638
KevinKerman1 <kevin.kerman01@gmail.com>
2024-08-10 16:48:37 -0700

    Element has appeared

---
+++ index.js
@@ -0,0 +1,26 @@
+<script>
+// Function to execute when the element appears
+function onElementAppears() {
+    console.log("Element has appeared!");
+    // Select the parent element using the provided selector
+
+
+    
+    // Place the action you want to perform here
+}
+
+// Function to check if the element is present in the DOM
+function checkForElement() {
+    const targetElement = document.querySelector("#vs6__combobox > div.vs__selected-options > span");
+    if (targetElement) {
+        onElementAppears();
+        observer.disconnect(); // Stop observing once the element has appeared
+    }
+}
+
+const observer = new MutationObserver(checkForElement);
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+</script>
+
