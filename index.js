1cdfcc724f7e761a78da41cb5878d928e16f0dfa
KevinKerman1 <kevin.kerman01@gmail.com>
2024-08-10 17:56:02 -0700

    -

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 function onElementAppears() {
     console.log("Element has appeared!");
     // Place the action you want to perform here
-   let Workflow = document.querySelector("#vs3__combobox > div.vs__selected-options");
-   Workflow.innerHTML = `
-   <span class="vs__selected">
-       Dialer
-       <!---->
-   </span> 
-   <input aria-autocomplete="list" aria-labelledby="vs3__combobox" aria-controls="vs3__listbox" type="search" autocomplete="off" class="vs__search">
-`;
+  // let Workflow = document.querySelector("#vs3__combobox > div.vs__selected-options");
+   //Workflow.innerHTML = `
+  // <span class="vs__selected">
+       //Dialer
+      // <!---->
+  // </span> 
+  // <input aria-autocomplete="list" aria-labelledby="vs3__combobox" aria-controls="vs3__listbox" type="search" autocomplete="off" class="vs__search">
+//`;
 console.log("Element has been modified!");
 }
 
