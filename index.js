5f63bc147888982960710ff568f3f9ee80417c1a
KevinKerman1 <kevin.kerman01@gmail.com>
2024-08-10 17:05:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Script Running from Git");
+// Function to execute when the element appears
+function onElementAppears() {
+    console.log("Element has appeared!");
+    // Select the parent element using the provided selector
+
+
+    
+    // Place the action you want to perform here
+}
+
+// Function to check if the element is present in the DOM
+function checkForElement() {
+    const targetElement = document.querySelector("#vs3__combobox > div.vs__selected-options");
+    if (targetElement) {
+        onElementAppears();
+        observer.disconnect(); // Stop observing once the element has appeared
+    }
+}
+
+const observer = new MutationObserver(checkForElement);
+
+observer.observe(document.body, { childList: true, subtree: true });
