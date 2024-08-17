dc611522223b8d28b1e85ea2d8290748e7d8ab2a
behzad05 <39502078+behzad05@users.noreply.github.com>
2024-08-18 01:29:38 +0500

    Update index.js
    Behzad Updated and deleted the Code

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// Function to execute when the element appears
-//function onElementAppears() {
-   // console.log("Element hass appeared!");
-     //Place the action you want to perform here
-  //let Workflow = document.querySelector("##__BVID__247___BV_modal_content_");
-   //Workflow.innerHTML = 
-
-   // Change the placeholder for the campaign select input
-//document.querySelector('[name="campaign"] .vs__selected-options .vs__selected').textContent = "Dialer";
-
-// Change the placeholder for the workflow select input
-//document.querySelector('[name="workflow"] .vs__selected-options .vs__selected').textContent = "Dialer";
-
-   
-//console.log("Element has been modified!");
-//}
-
-// Function to check if the element is present in the DOM
-//function checkForElement() {
-  //  const targetElement = document.querySelector("#vs3__combobox > div.vs__selected-options");
-  //  if (targetElement) {
-     //   onElementAppears();
-      //  observer.disconnect(); // Stop observing once the element has appeared
-//    }
-//}
-
-//const observer = new MutationObserver(checkForElement);
-
-//observer.observe(document.body, { childList: true, subtree: true });
-
-
-
-//Behzad Code Starts from here
 
 
 // Function to change the button text when it appears
