ec7847a3f858249e58b199a9aa6ccea7f6bd584f
behzad05 <39502078+behzad05@users.noreply.github.com>
2024-09-19 00:34:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -224,9 +224,9 @@ observeAddToAutomationButton();
             "#contact-details > div > div.hl_contact-details-right",
             "#contact-details > div > div.hl_contact-details-center > div.hl_conversations--message > div.hl_conversations--message-header-new",
             "#contact-details > div > div.hl_contact-details-center > div.hl_conversations--message > div.hl_conversations--message-body > div > div.message-input-wrap.overflowdisabled.new-channel-picker > div.flex.items-center > ul > li:nth-child(2)",
-            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(4)",
-            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(5)",
-            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(6)",
+            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(4) > div",
+            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(5) > div",
+            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(6) > div",
             "#contact-details > div > div.relative.p-0.hl_contact-details-left > div > div.h-full.overflow-y-auto > div.bg-gray-100",
             "#contact-details > div > div.relative.p-0.hl_contact-details-left > div > div.h-full.overflow-y-auto > div.px-4.pt-4.flex.justify-center.pb-4"
         ];
