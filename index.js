859f4b81bdf53e4961ac97cfd702e03c19959c4a
behzad05 <39502078+behzad05@users.noreply.github.com>
2024-09-18 23:55:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("script running");
-alert("test 60");
+alert("test 61");
 
 // Function to change the button text when it appears
 function changeButtonText(addToAutomationButton) {
@@ -221,7 +221,12 @@ observeAddToAutomationButton();
             "#tb_business",
             "#tb_contacts-settings-top",
             "#tb_contact-custom-fields-settings",
-            "#contact-details > div > div.hl_contact-details-right"
+            "#contact-details > div > div.hl_contact-details-right",
+            "#contact-details > div > div.hl_contact-details-center > div.hl_conversations--message > div.hl_conversations--message-header-new",
+            "#contact-details > div > div.hl_contact-details-center > div.hl_conversations--message > div.hl_conversations--message-body > div > div.message-input-wrap.overflowdisabled.new-channel-picker > div.flex.items-center > ul > li:nth-child(2)",
+            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(4)",
+            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(5)",
+            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(6)"
         ];
 
         selectors.forEach((selector, index) => {
