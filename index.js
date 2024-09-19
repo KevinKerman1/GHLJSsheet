f79e9e4b1126dfb6d329f1b521b503f842735ac2
behzad05 <39502078+behzad05@users.noreply.github.com>
2024-09-19 20:32:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("script running");
-alert("test 79");
+alert("test 80");
 
 // Function to change the button text when it appears
 function changeButtonText(addToAutomationButton) {
@@ -255,7 +255,7 @@ observeAddToAutomationButton();
             "#contact-details > div > div.hl_contact-details-center > div.hl_conversations--message > div.hl_conversations--message-header-new",
             "#contact-details > div > div.hl_contact-details-center > div.hl_conversations--message > div.hl_conversations--message-body > div > div.message-input-wrap.overflowdisabled.new-channel-picker > div.flex.items-center > ul > li:nth-child(2)",
             "#contact-details > div > div.hl_contact-details-center > div.hl_conversations--message > div.hl_conversations--message-body > div > div.message-input-wrap.overflowdisabled.new-channel-picker > div.flex.items-center > ul > li:nth-child(3)",
-            "#message-composer > div > div.flex.items-center.pt-2\.5.border-t > div:nth-child(1) > div:nth-child(5) > div",
+            "#message-composer > div > div.flex.items-center.pt-2\\.5.border-t > div:nth-child(1) > div:nth-child(5) > div",
             "#contact-details > div > div.relative.p-0.hl_contact-details-left > div > div.h-full.overflow-y-auto > div.bg-gray-100 > div:nth-child(5) > div",
             "#ContactsClientPortal > div",
             "#contact-details > div > div.relative.p-0.hl_contact-details-left > div > div.h-full.overflow-y-auto > div.bg-gray-100 > div:nth-child(2)",
